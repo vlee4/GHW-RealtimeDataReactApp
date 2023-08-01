c2212a0bf5f12e89a0907398a57c9ca57d0c4db2
Vic <60161691+vlee4@users.noreply.github.com>
2023-08-01 00:42:06 +0000

    Make fetch to polygon API for APPL stock info

--- realtime-currencies/src/app/page.js
+++ realtime-currencies/src/app/page.js
@@ -5,15 +5,16 @@ import {useState, useEffect} from 'react'
 
 
 export default function Home() {
-  const [pokeData, setPokeData] = useState({})
+  const [data, setData] = useState({})
 
 
   useEffect(()=>{
    async function getInfo(){
+    // const API_KEY = "nmgY9DpvofPAy6ntNUrKADFnTu8o2XmO"
       try{
-        const pokeData = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-        const jsonData = await pokeData.json()
-        setPokeData(jsonData)
+        const data = await fetch(`https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/minute/2023-01-09/2023-01-09?adjusted=true&sort=asc&limit=120&apiKey=nmgY9DpvofPAy6ntNUrKADFnTu8o2XmO`)
+        const jsonData = await data.json()
+        setData(jsonData)
         console.log(jsonData)
       } catch(err){
         console.error(err)
@@ -26,8 +27,8 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Hello World</h1>
-      <h2>{pokeData.name}</h2>
-      <img src={pokeData?.sprites?.front_default}style={{height: '400px'}}></img>
+      {/* <h2>{pokeData.name}</h2>
+      <img src={pokeData?.sprites?.front_default}style={{height: '400px'}}></img> */}
     </main>
   )
 }
